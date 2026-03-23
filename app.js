2bcc4a251ad4f3d11f57e8e58ea64a3fb1ac7926
vaibhavvora1 <vrvora11@gmail.com>
2026-03-23 19:54:43 +0530

    changes in backend

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ io.on("connection", function (uniquesocket) {
   });
 });
 
-server.listen(3000, function () {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
